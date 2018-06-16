4b4cea8388931c2c3c5ad5d05855034598093d58
unknown <letitslide87@gmail.com>
2018-06-16 10:53:04 -0400

    Browser support
    added browser support so people using old browsers still see images at a
    lower quality. Bypasses simpleIO completely.

--- simpleIO.js
+++ simpleIO.js
@@ -1,25 +1,38 @@
 //after the window loads
 window.addEventListener("load", function (event) {
-  createObserver();
+  var images = document.querySelectorAll('#lazy-img');
+  //if the browser doesn't support IO
+  if (!('IntersectionObserver' in window)) {
+    LoadImagesOldWay(images);
+  } else {
+    createObserver(images);
+  }
 }, false);
-function createObserver() {
+
+function createObserver(images) {
   var options = {
     root: null,
     rootMargin: "0px",
     threshold: .01
   };
   //select all elements with this id
-  var images = document.querySelectorAll('#lazy-img');
   var observer = new IntersectionObserver(handleIntersect, options);
-  images.forEach(function(image){
+  images.forEach(function (image) {
     observer.observe(image);
   });
 
 }
+//load the smallest image for old browsers
+function LoadImagesOldWay(images) {
+  images.forEach(function (image) {
+    var url = image.getAttribute('data-src');
+    image.setAttribute('src', url);
+  });
+}
 
 function handleIntersect(entries, observer) {
   // Loop through the entries
-  entries.forEach(function(entry){
+  entries.forEach(function (entry) {
     //if this entry is intersecting whatsoever
     if (entry.intersectionRatio > 0) {
       // Stop watching and load the image
@@ -28,7 +41,7 @@ function handleIntersect(entries, observer) {
       //we get our responsive image set
       var url = image.getAttribute('data-src');
       //and set them as the actual source
-      image.setAttribute('src',url);
+      image.setAttribute('src', url);
       //we unobserve (Separate) the image entirely
       observer.unobserve(entry.target);
       console.log('lazy-image loaded!:simpleIO.js line 35 to remove this!');
